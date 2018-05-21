281838016f068289184de0f54f353e7bb8fd2590
Mohamed Sami Mejri <medsamimejri@gmail.com>
2018-05-21 19:19:00 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'Senin.dart';
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Basic List',
+      home: new MyHomePage(),
+      routes: <String, WidgetBuilder>{
+        '/settings': (BuildContext context) => new SecondScreen(),
+      },
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Basic List';
+       return new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.map),
+              title: new Text('Map'),
+                onTap: (){Navigator.of(context).pushNamed("/mytabs");}
+            ),
+            new ListTile(
+              leading: new Icon(Icons.photo_album),
+              title: new Text('Album'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (context) => new SecondScreen()),
+                  );
+                }
+            ),
+            new ListTile(
+              leading: new Icon(Icons.phone),
+              title: new Text('Phone'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(builder: (context) => new SecondScreen()),
+                  );
+                }
+            ),
+          ],
+        ),
+      );
+  }
+}
